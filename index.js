dc9694f06b8d19d5a8ded2eef1106a6b54b2b1f3
meshackmesto <meshackorina93@gmail.com>
2024-03-19 18:53:18 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if (ride <= 400){
+  return 'This one is on me!'}
+  else if
+  (ride >400 && ride<=2000)
+    {return 'That will be twenty bucks.'}
+  else if(ride >2000 && ride <=2500){
+    return 'I will gladly take your thirty bucks.'
+  }
+  else{
+    return 'No can do.'
+  }
+    }
+function ternaryCheckCity(isCity){
+  // Write your code here!
+  return (isCity==="NYC") ? 'Ok, sounds good.' : "No go."
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip){
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
